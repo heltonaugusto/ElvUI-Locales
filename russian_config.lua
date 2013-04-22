1a832bcf2559bb32c5454e080436dd8bc1dccb72
Blazeflack <blazeflack@hotmail.com>
2013-04-22 12:00:43 +0200

    Test commit

--- russian_config.lua
+++ russian_config.lua
@@ -3,6 +3,7 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "ruRU")
 if not L then return; end
 
+--Test comment
 --ActionBars
 L["Action Paging"] = "Переключение панелей"
 L["ActionBars"] = "Панели команд"
